9548029865727b4c46f2719775b51029940985de
evan <evan30anderson@gmail.com>
2020-04-26 16:31:23 -0500

    de-modularized version of tutorial code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,99 @@
-var testImport = require('test.js')
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+	    if(creep.store.getFreeCapacity() > 0) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+        }
+	}
+};
+
+var roleBuilder = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.building = false;
+            creep.say('🔄 harvest');
+	    }
+	    if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.building = true;
+	        creep.say('🚧 build');
+	    }
+
+	    if(creep.memory.building) {
+	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+	    }
+	    else {
+	        var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+	    }
+	}
+};
+
+var roleUpgrader = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+      // if the creep's energy storage is empty
+	    if(creep.store[RESOURCE_ENERGY] == 0) {
+
+            // find energy sources
+            var sources = creep.room.find(FIND_SOURCES);
+
+            // move closer if energy source is out of range
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+        }
+
+        // if storage is not empty
+        else {
+
+            // move towards the controller
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+            }
+        }
+	}
+};
 
 export const loop = function() {
-    console.log("Hello Phineas!");
+  for(var name in Game.creeps) {
+      var creep = Game.creeps[name];
+      if(creep.memory.role == 'harvester') {
+          roleHarvester.run(creep);
+      }
+      if(creep.memory.role == 'upgrader') {
+          roleUpgrader.run(creep);
+      }
+      if(creep.memory.role == 'builder') {
+          roleBuilder.run(creep);
+      }
+  }
 }
